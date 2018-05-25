51de09d1be8d9d48e7d00780d809388501c40ba7
Dominik Moritz <domoritz@gmail.com>
2018-05-25 16:37:25 -0700

    Modularize the build

--- compile_web.sh
+++ compile_web.sh
@@ -40,7 +40,7 @@ emcmake cmake \
         -DCLASP_BUILD_WITH_THREADS=Off \
         -DCMAKE_VERBOSE_MAKEFILE=On \
         -DCMAKE_BUILD_TYPE=release \
-        -DCMAKE_CXX_FLAGS="-s ALLOW_MEMORY_GROWTH=1" \
+        -DCMAKE_CXX_FLAGS="-s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1" \
         -DCMAKE_EXE_LINKER_FLAGS="" \
         -DCMAKE_EXE_LINKER_FLAGS_RELEASE="" \
         ../..
