996f7b56fac7a3cb7a8fe280cd952924e98f0e2a
Dominik Moritz <domoritz@gmail.com>
2018-10-02 21:25:57 -0700

    Rebuild with latest emscripten

--- docs/serve.py
+++ docs/serve.py
@@ -13,7 +13,7 @@ Handler.extensions_map.update({
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
 
-print(f"Serving HTTP with WASM support on 0.0.0.0 port {PORT} (http://0.0.0.0:{PORT}/) ...")
+print(f"Serving HTTP with WebAssembly support on 0.0.0.0 port {PORT} (http://0.0.0.0:{PORT}/) ...")
 try:
     httpd.serve_forever()
 except KeyboardInterrupt:
