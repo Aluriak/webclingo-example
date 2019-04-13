9df882b610be14f930c855a32d9507228237b3b6
flavioeverardo <flavio.everardo@cs.uni-potsdam.de>
2019-04-13 08:29:29 +0200

    test with python

--- compile_web.sh
+++ compile_web.sh
@@ -4,21 +4,24 @@ set -e
 set -x
 
 # install emscripten (copy-paste of documentation)
-mkdir -p emscripten
-cd emscripten
-wget http://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz -O emsdk-portable.tar.gz
-tar -xvf emsdk-portable.tar.gz
-cd emsdk-portable
+#mkdir -p emscripten
+#cd emscripten
+#wget http://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz -O emsdk-portable.tar.gz
+#tar -xvf emsdk-portable.tar.gz
+#cd emsdk-portable
+#git clone https://github.com/emscripten-core/emsdk.git
+cd emsdk
+git pull
 
 # Fetch the latest registry of available tools.
-./emsdk update
+#./emsdk update
 # Download and install the latest SDK tools.
 ./emsdk install latest
 # Make the "latest" SDK "active" for the current user. (writes ~/.emscripten file)
 ./emsdk activate latest
 # Activate PATH and other environment variables in the current terminal
 source ./emsdk_env.sh
-cd ../..
+cd ..
 
 # compile lua
 unzip -o lua.zip -d lua
@@ -39,9 +42,11 @@ git submodule update --init --recursive
 mkdir -p build/web
 cd build/web
 
-sudo emcmake cmake \
+emcmake cmake \
         -DCLINGO_BUILD_WEB=On \
         -DCLINGO_BUILD_WITH_PYTHON=On \
+		-DPYTHON_INCLUDE_DIR="/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7" \
+		-DPYTHON_LIBRARY="/Users/flavioeverardo/miniconda2/lib/libpython2.7.dylib" \
         -DLUA_INCLUDE_DIR="$(pwd)/../../../lua/install/include" \
         -DLUA_LIBRARIES="$(pwd)/../../../lua/install/lib/liblua.a" \
         -DCLINGO_BUILD_WITH_LUA=On \
